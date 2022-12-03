85994a0cac51007c869e205e2577a4a8ca784104
aylingunal <33787710+aylingunal@users.noreply.github.com>
2022-12-03 12:43:15 -0500

    Update GraphBuilder.py

--- GraphBuilder.py
+++ GraphBuilder.py
@@ -2,6 +2,7 @@
 import networkx as nx
 from nltk.tokenize import word_tokenize, sent_tokenize
 import matplotlib.pyplot as plt
+from helpers import *
 
 # given an input text, create a text graph given
 # specifications provided by args (type dict)
@@ -25,24 +26,38 @@ def build_text_graph(raw_text, args):
         attributes = {ind:{'token':tokens[ind]}}
         nx.set_node_attributes(graph, attributes)
 
+
+    ### ADD EDGES ###
     # add edges to the graph
-    for ind in range(len(tokens) - 1):
-        # assuming undirected graph, only assigning one way
-        graph.add_edge(ind, (ind+1))
-        # add edge attributes
-        attributes = {(ind, ind+1):{'cosine_sim':0.0}}
-        nx.set_edge_attributes(graph, attributes)
+    for ind in range(len(tokens)):
+        # get cos sim between every pair of nodes for now
+        for ind2 in range(len(tokens)):
+            # don't compare node to itself, and assuming undirected graph so don't add (u,v),(v,u) both
+            if ((ind != ind2) and (graph.has_edge(ind2, ind) == False)):
+                graph.add_edge(ind, ind2)
+                # add edge attributes
+                cosine_sim = compute_cosine_sim(tokens[ind],tokens[ind2])
+                print(cosine_sim)
+                attributes = {(ind, ind2):{'weight':cosine_sim,'weight_type':'cosine_sim'}}
+                nx.set_edge_attributes(graph, attributes)
+
+    # TODO remove edges w/ sim weights below a certain threshold
 
     nx.draw(graph)
     plt.show()
     return graph
 
-def main():
-    raw_text = "hello world! I am here"
-    args = {'TokenType':'sentence', 'EdgeType':'single'}
-    build_text_graph(raw_text,args)
+# def main():
+#     raw_text = "Premium composition leather exterior and soft interior offer great protection against daily use; Classic and professional design, solid construction\
+#                 Support auto Sleep/Wake feature; Cover features magnetic closure; Features a large front document card pocket to keep personal belongings\
+#                 Full access to all features (Cameras, Speaker, Ports and Buttons); Multiple slots able to set up multiple horizontal stand angles\
+#                 Built-in elastic pencil holder for Apple Pencil or stylus"
+#     args = {'TokenType':'word', 'EdgeType':'single'}
+#     graph = build_text_graph(raw_text,args)
 
 
-if __name__ == '__main__':
-    main()
+
+
+# if __name__ == '__main__':
+#     main()
 
